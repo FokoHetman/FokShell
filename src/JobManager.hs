dd277421127c98b52787b3a20e3752f31435fda8
FokoHetman <paprykkania@gmail.com>
2025-10-05 19:11:27 +0200

    parsing rewrite

--- src/JobManager.hs
+++ src/JobManager.hs
@@ -19,63 +19,20 @@ handleJob (ShellProcess conf state) = do
   putStrLn $ "Job Manager handling: " ++ T.unpack (input conf)
   
   --print $ runParser parseExpr $ input conf
-  let nodes = getNodes $ input conf
-  print nodes
 
-  let unodes = cognifyNodes nodes
-  print unodes
-  let string = stringify unodes
-  print string
-
+  --let string = stringify unodes
+  --print string
   
-
-  --let args = tree $ input conf
-  --print args
-
+  --evaluateNodes (ShellProcess conf state) unodes
   pure (ShellProcess conf state)
 
-evaluateNodes :: ShellProcess -> [Node] -> IO ()
-evaluateNodes (ShellProcess conf state) ((String text node):nodes) = undefined--do
-    --asText node -- rethink this.
 
+data StringComplex = Basic T.Text | Variant [StringComplex] | Combination [StringComplex]
+  deriving (Show,Eq)
+type Executable = StringComplex
+type Args       = [StringComplex]
 
-stringify :: Maybe [Node] -> Maybe [T.Text]
-stringify nodes = map f <$> nodes
-  where
-    f :: Node -> T.Text
-    f (String t _) = t
-    f (Variant v _) = T.intercalate " " $ fmap f v
-
-cognifyNodes :: Maybe [Node] -> Maybe [Node]
-cognifyNodes nodes = map walkNode <$> nodes
-
--- integrate EnvVar and Call(?) here
-walkNode :: Node -> Node
-walkNode = \case
-  String t n -> case walkNode n of 
-    String t2 n2 -> walkNode (String (T.concat [t,t2]) n2)
-    Variant v n2 -> walkNode (Variant (fmap (walkNode . String t) v) n2)
-    EOL          -> String t n
-  Variant v n -> case walkNode n of
-    String t n2 -> walkNode (Variant (fmap (walkNode . (injectEnd (String t EOL))) v) n2)
-    Variant v2 n2 -> walkNode (Variant [walkNode (injectEnd (Variant v2 EOL) x)|x<-v] n2)
-    EOL -> Variant v n
-  x -> x 
-
-injectEnd :: Node -> Node -> Node
-injectEnd i (String t n) = String t $ injectEnd i n
-injectEnd i (Variant v n) = Variant v $ injectEnd i n
-injectEnd i EOL = i
-
-getNodes :: T.Text -> Maybe [Node]
-getNodes t = if t == T.empty then Just [] else do
-  (r, n :: Node) <- runParser parseExpr $ T.strip t
-  
-  trace ("rest: " ++ T.unpack r ++ " and n: " ++ show n) getNodes r >>= f n
-  where
-    f x y = Just (x:y)
-
-data Node = String T.Text Node | Call Node | Variant [Node] Node | EnvVar T.Text Node | Space | EOL
+data Node = ProgramCall Executable Args
   deriving (Show,Eq)
 
 newtype Parser a = Parser {runParser :: T.Text -> Maybe (T.Text, a)}
@@ -98,15 +55,70 @@ instance Alternative Parser where
   (Parser p1) <|> (Parser p2) = Parser $ \input -> p1 input <|> p2 input
 
 
-tree :: T.Text -> Node
-tree = undefined
+
+
+parseExpr :: Parser Node
+parseExpr = trace "parseExpr" $ pcall -- <|> andand
+
+
+ws :: Parser T.Text
+ws = spanP isSpace
+
+sepBy :: Parser a
+      -> Parser b
+      -> Parser [b]
+sepBy sep element = (:) <$> element <*> many (sep *> element) <|> pure []
+
+
+pcall :: Parser Node
+pcall = ProgramCall . capStr
+        <$> (ws *> extractUntil " " <* charP ' ' <* ws)
+        <*> many (capStr <$> (ws *> extractUntil " " <* charP ' ' <* ws))
+
+captureFullString :: Parser StringComplex
+--captureFullString = Combination <$> many stringify
+captureFullString = undefined -- loop over or smth I lost a will to live idk
+
+capStr :: T.Text -> StringComplex
+capStr t = f (runParser stringify t)
+  where 
+    f (Just t) = case t of
+      ("", x) -> x
+      (r,  x) -> case capStr r of
+        Combination y -> Combination (x:y)
+        y             -> Combination [x, y]
+
+stringify :: Parser StringComplex
+stringify = basic <|> variant
+
+
+
+basic :: Parser StringComplex
+basic = Basic <$> extractUntil " $(){,}"
+
+variant :: Parser StringComplex
+variant = Variant <$> (charP '{' *> ws *> variants <* ws <* charP '}')
+  where
+    variants = sepBy (ws *> charP ',' <* ws) stringify
+
+extractUntil :: [Char] -> Parser T.Text
+extractUntil b = Parser f'
+  where
+    f' t = trace ("extract: `" ++ T.unpack t ++ "`") result
+      where
+        parsed = f t
+        rest = trace ("parsed: " ++ show parsed) T.takeEnd (T.length t - T.length parsed) t
+        result =  if T.null parsed then Nothing else Just (rest, parsed)
+    f t
+      | T.null t          = trace "works" T.empty
+      | T.head t `elem` b = trace "work2" T.empty
+      | otherwise         = T.concat [T.singleton $ T.head t, f (T.drop 1 t)]
+
+
+{-
 
 parseExpr :: Parser Node
 parseExpr = trace "called" $ eol <|> strings <|> envVars <|> variants
-
-
---strings :: Parser Node
---strings = String <$> (charP ' ' *> spanP (/=' ') <* charP ' ') <*> parseExpr
 
 openers = ['{', '(', '"', '$']
 openers, limited, blockers :: [Char]
@@ -160,6 +172,9 @@ extractChar c blockers
   | c `elem` blockers = Nothing
   | otherwise = Just c
 
+
+-}
+
 charP :: Char -> Parser Char
 charP x = Parser f
   where
@@ -177,6 +192,3 @@ spanP :: (Char -> Bool) -> Parser T.Text
 spanP f = Parser $ \input ->
   let (token, rest) = T.span f input
   in Just (rest, token)
-
-argumentize :: Node -> [T.Text]
-argumentize = undefined
