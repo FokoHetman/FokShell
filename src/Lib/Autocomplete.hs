2f3a9b178526c004184bff8bcc6f8fc06c1550ef
FokoHetman <paprykkania@gmail.com>
2025-11-07 18:50:19 +0100

    Fully working executable autocomplete

--- src/Lib/Autocomplete.hs
+++ src/Lib/Autocomplete.hs
@@ -5,7 +5,7 @@ import qualified Data.Text as T
 import Lib.Primitive
 import Lib.Format
 import Lib.ColorScheme
-import Data.Maybe (isJust)
+import Data.Maybe (isJust, fromMaybe, isNothing)
 import Data.Functor
 import System.Directory (findExecutable, getCurrentDirectory, getDirectoryContents, getPermissions, Permissions (executable), doesDirectoryExist)
 import Control.Monad (when, liftM, filterM, join, unless)
@@ -15,6 +15,8 @@ import Debug.Trace (traceShow)
 import System.FilePath.Posix ((</>))
 import Data.Char (isSpace)
 import Data.Bool (bool)
+import Control.Arrow (Arrow(first))
+import Data.Foldable (for_)
 
 getDirsInPath :: IO [FilePath]
 getDirsInPath = filterM doesDirectoryExist . fmap T.unpack . T.split (==':') . T.pack =<< getEnv "PATH"
@@ -45,7 +47,7 @@ defaultModel inp cursor hist = if isMatchingExecutable then do
     --print pathExecs
     --print("over")
 
-    let matches = filter (T.isPrefixOf inp) $ fmap T.pack $ localExecutables ++ pathExecs
+    let matches = filter (T.isPrefixOf fstWord) $ fmap T.pack $ localExecutables ++ pathExecs
     pure (matches, wholeMatches)
   else do 
     --print fstWord
@@ -53,30 +55,85 @@ defaultModel inp cursor hist = if isMatchingExecutable then do
     pure ([], wholeMatches)
   where
     pathExecutables = mapM executablesInDir =<< getDirsInPath
+
+    (matchIndex, cursorIndex) = extractIndexes inp cursor
+    curWord = case matchIndex of
+      Just x -> T.words inp !! x
+      Nothing-> ""
     fstWord = fstWord' $ T.words inp
     fstWord' (x:__) = x
     fstWord' [] = ""
-    curWord = curWord' cursor $ reverse $ T.words inp
-    curWord' i (x:xs) = if T.length x > i then x else curWord' (i-T.length x) xs
-    curWord' i _ = ""
-    isMatchingExecutable = curWord == fstWord
+    isMatchingExecutable = matchIndex == Just (length (T.words inp)-1)
 
     wholeMatches = filter (T.isPrefixOf inp) hist
 
 
 -- stop using fstWord == curWord you fucking piece of shit it makes the thing break when `command command`
 
+extractIndexes text cursor = case f cursor (T.reverse text) of
+      Nothing -> (Nothing, Nothing)
+      Just (a,b) -> bool (Nothing, Nothing) (Just a, Just b) (a<length ( T.words text))
+      where
+        f :: Int -> T.Text -> Maybe (Int, Int)
+        f cursor text
+          | T.null text = Just (0, 0)
+          | T.head text == ' ' = Nothing
+          | and [T.length (head $ T.words text) > cursor, not . isSpace $ T.head text] = Just (0, cursor)
+          | otherwise =  (T.stripPrefix (head $ T.words text) text) 
+            >>= (\x -> (bool (f (cursor - T.length (head $ T.words text) - countLeadingWhitespace x) (T.strip x)) Nothing (countLeadingWhitespace x > cursor - T.length (head $ T.words text))))
+            <&> first (1+)
+
+-- when cursorIndex == matchedLength, redraw entire prompt
 
 defaultHook :: T.Text -> Int -> ColorScheme -> ([T.Text], [T.Text]) -> IO ()
 defaultHook t i c m = unless (T.null t) $
-    when (i >= T.length t - T.length fstWord) (resetCursor>> displayExecutable >> moveToCursor)
+    bool
+      ({-when (i >= T.length t - T.length fstWord) re-add I thibk-}resetCursor>> displayExecutable >> moveToCursor >> toEOW >> displayPrediction >> displayRight >> retrieveRight >> retrievePrediction >> fromEOW)
+      ({-traceShow ("left: `" <> left <> "`:" <> "right: `" <> right <> "`:") $ -}resetCursor >> putStrf "\ESC[0K" >> displayExecutable >> displayLeft >> displayCurrent >> displayPrediction >> displayRight >> retrieveCursor >> retrievePrediction)
+      (isNothing matchIndex)
   where
     fstWord = head $ T.words t
 
-    rest = bool "" " " (T.length f>0) <> f
-      where
-        f = T.concat $ tail $ T.words t
+    {-toEOW = when (cursorIndex > 0) $ 
+        moveCursor DRight cursorIndex
+    fromEOW = when (cursorIndex > 0) $
+        moveCursor DLeft cursorIndex-}
+    (toEOW, fromEOW) = case cursorIndex of
+      Just x -> (when (x > 0) $ moveCursor DRight x,
+                 when (x>0) $ moveCursor DLeft x)
+      Nothing -> (pure (), pure ())
+    (matchIndex, cursorIndex) = extractIndexes t i
+
+    curWord = matchIndex <&> (reverse (T.words t)!!) 
+
+    displayCurrent = for_ curWord putStrf
+
+    pred = case fst m of 
+      (x:_) -> T.stripPrefix fstWord x
+      [] -> Nothing
+    --displayPrediction = putStrf pred
+    (displayPrediction, retrievePrediction) = case pred of 
+      Just p -> (shadowText c >>= putStrf . (<>p<>"\ESC[0m") . asciiColor, when (T.length p>0) $ moveCursor DLeft $ T.length p)
+      Nothing -> (pure (), pure ())
+
+    (curWordLeft, curWordRight) = case (curWord, cursorIndex) of
+      (Just x, Just y) -> (T.take (T.length x - y) x, T.reverse $ T.take y $ T.reverse x)
+      _ -> ("", "")
+    displayRest = for_ (T.stripPrefix fstWord $ T.stripStart t) putStrf
+
+    left = bool "" (unwrap $ T.stripPrefix fstWord $ T.take (T.length t - i - T.length curWordLeft) t) (T.length t > i)
+    displayLeft = putStrf left
+
+    -- todo: strip if fstWord is the prefix
+    right = bool r (unwrap $ T.stripPrefix fstWord r) (fstWord `T.isPrefixOf` r)  where r = T.reverse $ T.take (i - T.length curWordRight) $ T.reverse t
+    displayRight = putStrf right
+    retrieveRight = when (T.length right>0) $ moveCursor DLeft $ T.length right
+    rest = fromMaybe "" $ T.stripPrefix fstWord $ T.stripStart t
+    
     resetCursor = when (T.length t > i) $ moveCursor DLeft (T.length t - i)
+
+    retrieveCursor = when (r > 0) $ moveCursor DLeft r where r = i
+
 
     formattedWord e = (if e || T.null fstWord then successColor c else errorColor c) <&> (<>fstWord<>"\ESC[0m") . asciiColor
     displayExecutable = (findExecutable (T.unpack fstWord) >>= formattedWord . isJust) >>= putStrf
@@ -95,6 +152,7 @@ instance Def AutocompleteConfig where
   , redrawHook = defaultHook
   }
 
+-- horrible sin
 unwrap (Just x) = x
 
 countLeadingWhitespace :: T.Text -> Int
